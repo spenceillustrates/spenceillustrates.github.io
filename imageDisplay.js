0264d29e6e716fb7412b8c4076b6b215e13d95d3
Rhys Povey <rhys.povey82@gmail.com>
2025-07-07 15:22:58 +0100

    row structure finished, changes to all CSS

--- imageDisplay.js
+++ imageDisplay.js
@@ -26,50 +26,76 @@ async function getImageWithIndex(path, folderPath, idx) {
 }
 
 // for index.html
-async function populateImageColumns() {
-    const lc = document.getElementById("leftColumn")
-    const rc = document.getElementById("rightColumn")
-    let lcHeight = 0;
-    let rcHeight = 0;
-
-    let res = await loadJSON(mediaPath+"artworks.json");
-	res = res.reverse()
-    let addToLeft = true
-	for (let i = 0; i < res.length; i++) {
-		let newImg = document.createElement("img");
-        newImg.classList.add("fade");
-			
-		let newText = document.createElement("a");  // image title (text not added until image is loaded)
-		newText.classList.add("overlayed")
-			
-		let newPiece = document.createElement("div");   // div wrapper
-        newPiece.append(newText);
-        newPiece.onclick = function() {     // for changing to single view
-			window.localStorage.setItem('title', (res[i].title+""));
-            window.localStorage.setItem('folder', (res[i].folder+""));
-            window.location.href=("artwork.html");
-        };
+async function populateImageRows() {
+	const cc = document.getElementById("centralColumn");
+	let res = await loadJSON(mediaPath+"artworks.json");
+	
+	let rowDiv = document.createElement("div");
+	rowDiv.classList.add("rowContainer");
+	for (let c = 0; c < res.length; c++){
+		// row logic
+		let currentCollage = res[c]["collage"];
+		let spaceDiv = document.createElement("div");
+		spaceDiv.style.width = "100%";
+		spaceDiv.style.height = "50px";
+		cc.append(spaceDiv);
 		
-		if (addToLeft) {lc.append(newPiece);}
-		else {rc.append(newPiece);}
-		addToLeft = !addToLeft
+		for (let i = 0; i < currentCollage.length; i++) {
+			if (i % 2 == 0) {
+				rowDiv = document.createElement("div");
+				rowDiv.classList.add("rowContainer");
+			}
+			
+			let newImg = document.createElement("img");
+			newImg.classList.add("fade");
+			
+			let newText = document.createElement("a");  // image title (text not added until image is loaded)
+			newText.classList.add("overlayed")
+			
+			let newPiece = document.createElement("div");   // div wrapper
+			newPiece.append(newText);
+			newPiece.onclick = function() {     // for changing to single view
+				window.localStorage.setItem('title', (currentCollage[i].title+""));
+				window.localStorage.setItem('folder', (currentCollage[i].folder+""));
+				window.location.href=("artwork.html");
+			};
 		
-		selectedIndex = 0
-		if (res[i].hasOwnProperty('altIndex')) {
-			selectedIndex = res[i].altIndex
+			if (i == currentCollage.length-1 && currentCollage.length % 2 == 1) { // final image doesn't fit into row, center
+				let tmpDiv = document.createElement("div");
+				tmpDiv.style.float = "left";
+				tmpDiv.style.width = "25%";
+				rowDiv.append(tmpDiv);
+				newPiece.style.float = "left";
+			} 
+			else if (i % 2 == 0) {	// first in the row, left
+				newPiece.style.float = "left";
+			}
+			else {	// right
+				newPiece.style.float = "right";
+			}
+			rowDiv.append(newPiece);
+			
+			// final row logic
+			if (i % 2 == 1 || i == currentCollage.length-1) {
+				cc.append(rowDiv);
+			}
+		
+			selectedIndex = 0
+			if (currentCollage[i].hasOwnProperty('altIndex')) {
+				selectedIndex = currentCollage[i].altIndex
+			}
+			getImageWithIndex(mediaPath, currentCollage[i].folder, selectedIndex) // we only need to load the first image from the artwork folders. The rest is shown on the artwork page
+			.then(res => res.blob())
+			.then(data => {
+				newImg.src = URL.createObjectURL(data);
+				newImg.onload = function() {    // add image to div once loaded
+					newText.innerHTML = currentCollage[i].title;
+					newPiece.append(newImg);
+				}
+			})
 		}
-		else {
-		}
-		getImageWithIndex(mediaPath, res[i].folder, selectedIndex) // we only need to load the first image from the artwork folders. The rest is shown on the artwork page
-        .then(res => res.blob())
-        .then(data => {
-			newImg.src = URL.createObjectURL(data);
-			newImg.onload = function() {    // add image to div once loaded
-				newText.innerHTML = res[i].title;
-				newPiece.append(newImg);
-            }
-        })
-    }
+			
+	}
 }
 
 // for artwork.html
